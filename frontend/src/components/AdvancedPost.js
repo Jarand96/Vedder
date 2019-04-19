43cb501943d7fa20f0bc605aeedaf3fe3fc100bf
Jarand Nikolai Jansen <jarandjansen182@gmail.com>
2019-04-19 17:43:27 +0200

    Wrote algorithm to check for freeSpace in grid

--- frontend/src/components/AdvancedPost.js
+++ frontend/src/components/AdvancedPost.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import {store} from "react";
 import { Link } from "react-router";
 import { connect } from 'react-redux';
+var randomColor = require('randomcolor');
 
 
 class AdvancedPost extends Component {
@@ -12,6 +13,52 @@ class AdvancedPost extends Component {
       grid : [],
     }
     this.addDiv = this.addDiv.bind(this);
+  }
+
+  isSpaceFree(column_start, width, row_start, height){
+    let spaceIsFree = true;
+    let grid = this.state.grid;
+    let column_end = column_start + width;
+    let row_end = row_start + height;
+    grid.forEach((object, index)=> {
+      //Check if object is on the same columns as the free space
+      //If that is the case, we have to check if the object is also on
+      //the same row as the space we are checking.
+      let sharedColumn = false;
+      //Check if there is already an object starting in that column
+      if(column_start === object.column_start){
+          sharedColumn = true;
+      }
+      //Check if the space we are checking for is on the left side of the object.
+      //And then checks if the object intercepts with the space we are checking.
+      else if(column_start < object.column_start){
+          if(!column_end<=object.column_start){
+            sharedColumn = true;
+          }
+      }
+      //The space we are checking is on the right side of current object
+      else{
+          if(!column_start>=object.column_end){
+            sharedColumn = true;
+          }
+      }
+      //If shared column is true, then we have to check if the object is on the same row also
+      if(sharedColumn){
+        if(row_start === object.row_start){
+          spaceIsFree = false;
+          }
+        else if(row_start < object.row_start){
+          //The freespace is intercepting with the objects bounds
+          if(!row_end <= object.row_start)
+            spaceIsFree = false;
+        }
+        else{
+          if(!row_start >= object.row_end)
+            spaceIsFree = false;
+        }
+      }
+    })
+    return spaceIsFree
   }
 
   getGridHeight(){
@@ -26,12 +73,18 @@ class AdvancedPost extends Component {
   }
 
   addDiv(){
-    let grid = this.state.grid
+    let grid = this.state.grid;
+    let grid_height = this.getGridHeight();
+    let freeSpace = this.isSpaceFree(1,12,grid_height,1);
+    console.log(freeSpace)
+    if(!freeSpace){
+      grid_height += 1;
+    }
     grid.push({
-      "width": 6,
+      "width": 12,
       "height":1,
-      "column_start": 4,
-      "row_start":1,
+      "column_start": 1,
+      "row_start": grid_height,
       "text" : "Hei på deg, jeg er en div.",
       "isPlaceholder" : false,
     })
@@ -57,7 +110,7 @@ class AdvancedPost extends Component {
               gridColumnEnd: object.column_start + object.width,
               gridRowStart: object.row_start,
               gridRowEnd: object.row_start + object.height,
-              backgroundColor : "red"
+              backgroundColor : randomColor()
             }
             return (<div key={index} style={div_style}>{object.text}</div>)
           })}
