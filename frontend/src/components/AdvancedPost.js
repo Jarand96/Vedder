2d4b9605a177264035e11afd7e535a4b73abecbd
Jarand Nikolai Jansen <jarandjansen182@gmail.com>
2019-04-20 10:33:01 +0200

    Fixed error in logical operators in getFreespace algorithm

--- frontend/src/components/AdvancedPost.js
+++ frontend/src/components/AdvancedPost.js
@@ -28,35 +28,55 @@ class AdvancedPost extends Component {
       let sharedColumn = false;
       //Check if there is already an object starting in that column
       if(column_start === object.column_start){
+          console.log("Object and freespace starts on the same column.")
           sharedColumn = true;
       }
       //Check if the space we are checking for is on the left side of the object.
       //And then checks if the object intercepts with the space we are checking.
       else if(column_start < object.column_start){
-          if(!column_end<=object.column_start){
+          console.log("Free space start is lower than the objects start")
+          if(column_end>object.column_start){
+            console.log("The freespace end at column: " + column_end)
+            console.log("The object starts at column: " + object.column_start)
+            console.log("Free space end is not lower than the objects start, that means they are sharing space.")
             sharedColumn = true;
           }
       }
       //The space we are checking is on the right side of current object
       else{
-          if(!column_start>=object.column_end){
+        console.log("Free space start is bigger than objects start.")
+          if(column_start<object.column_end){
+            console.log("Objects end is bigger than free space start, meaning they share column.")
             sharedColumn = true;
           }
       }
+
+      console.log("Column space check. Free space and object share column: " + sharedColumn)
       //If shared column is true, then we have to check if the object is on the same row also
       if(sharedColumn){
         if(row_start === object.row_start){
+          console.log("SHARED COLUMN AND SAME ROW START")
+          console.log("Failed because object with column start: " + object.column_start)
           spaceIsFree = false;
           }
         else if(row_start < object.row_start){
           //The freespace is intercepting with the objects bounds
-          if(!row_end <= object.row_start)
-            spaceIsFree = false;
+          if(row_end > object.row_start){
+            console.log("FREE SPACE DOES NOT END BEFORE NEXT OBJECT STARTS")
+            console.log("Failed because object with row start: " + object.row_start)
+            console.log("Free space ends at: " + row_end + ", Object starts at: " + object.row_start)
+            spaceIsFree = false;}
         }
+        //If freespace start is bigger than object start
         else{
-          if(!row_start >= object.row_end)
+          if(row_start < object.row_end){
+            //Freespace start must be bigger or equal to the end of object, if not:
+            console.log("OBJECT DOES NOT END BEFORE START OF FREESPACE")
+            console.log("Failed because object with row start: " + object.row_start)
+            console.log("Object ends at: " + object.row_end + ", Free space starts at: " + row_start)
             spaceIsFree = false;
         }
+      }
       }
     })
     return spaceIsFree
@@ -76,16 +96,21 @@ class AdvancedPost extends Component {
   addDiv(){
     let grid = this.state.grid;
     let grid_height = this.getGridHeight();
-    let freeSpace = this.isSpaceFree(1,12,grid_height,1);
-    console.log(freeSpace)
+    let column_start = parseInt(this.state.column_start);
+    let row_start = parseInt(this.state.row_start);
+    let width = parseInt(this.state.width)
+    let height = parseInt(this.state.height)
+    let freeSpace = this.isSpaceFree(column_start,width,row_start,height);
+
+    console.log("Is space free: " + freeSpace)
     if(!freeSpace){
-      grid_height += 1;
+      row_start = grid_height + 1;
     }
     grid.push({
-      "width": parseInt(this.state.width),
-      "height":1,
-      "column_start": 1,
-      "row_start": grid_height,
+      "width": width,
+      "height": height,
+      "column_start": column_start,
+      "row_start": row_start,
       "text" : "Hei på deg, jeg er en div.",
       "isPlaceholder" : false,
       "backgroundColor" : randomColor()
@@ -101,13 +126,11 @@ class AdvancedPost extends Component {
     let grid_style = {
       gridTemplateRows: `repeat(${grid_height}, 40px)`
     }
-    console.log(this.state)
     return(
     <div className="advanced_post">
         {grid.length>0 &&
           <div className = "grid-container" style={grid_style}>
           {grid.map((object, index) => {
-            console.log(object);
             let div_style = {
               gridColumnStart: object.column_start,
               gridColumnEnd: object.column_start + object.width,
@@ -117,17 +140,38 @@ class AdvancedPost extends Component {
             }
             return (<div onClick={() => {
               this.setState({
-                'objectInFocus' : object 
+                'objectInFocus' : object
               })
             }} key={index} style={div_style}>{object.text}</div>)
           })}
         </div>
         }
-        <div>
+        <div className="input-group">
         <label>width</label>
         <input type='text' name="width" onChange={(e) => {
           this.setState({
             'width':e.target.value
+        })}}/>
+        </div>
+        <div className="input-group">
+        <label>height</label>
+        <input type='text' name="height" onChange={(e) => {
+          this.setState({
+            'height':e.target.value
+        })}}/>
+        </div>
+        <div className="input-group">
+        <label>Column start</label>
+        <input type='text' name="column_start" onChange={(e) => {
+          this.setState({
+            'column_start':e.target.value
+        })}}/>
+        </div>
+        <div className="input-group">
+        <label>Row start</label>
+        <input type='text' name="row_start" onChange={(e) => {
+          this.setState({
+            'row_start':e.target.value
         })}}/>
         </div>
         <button onClick={this.addDiv}>Add div</button>
