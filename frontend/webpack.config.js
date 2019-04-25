aef3f4955f9dbda9f44d7a9c700ceaa31001d1b9
Jarand Nikolai Jansen <jarandjansen182@gmail.com>
2019-04-26 00:43:36 +0200

    Inserted own webpack config

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,53 +1,33 @@
-// http://webpack.github.io/docs/configuration.html
-// http://webpack.github.io/docs/webpack-dev-server.html
-var app_root = 'src'; // the app root folder: src, src_users, etc
-var path = require('path');
-var CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  app_root: app_root, // the app root folder, needed by the other webpack configs
-  entry: [
-    // http://gaearon.github.io/react-hot-loader/getstarted/
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
-    'babel-polyfill',
-    __dirname + '/' + app_root + '/index.js',
-  ],
+  entry: './src/index.js',
+
   output: {
-    path: __dirname + '/public/js',
-    publicPath: 'js/',
+    path: path.resolve(__dirname, '/public/js/'),
     filename: 'bundle.js',
+    publicPath: '/'
   },
+
   module: {
-    loaders: [
-      {
+    rules: [{
         test: /\.js$/,
-        loader: ['babel-loader'],
-        exclude: /node_modules/,
-        query:{
-          presets: ['react','es2015'],
-          plugins: ['transform-class-properties']
-        }
+        use: 'babel-loader'
       },
+
       {
-        // https://github.com/jtangelder/sass-loader
         test: /\.scss$/,
-        loaders: ['style', 'css', 'sass'],
-      },
-      {
-        test: /\.css$/,
-        loaders: ['style', 'css'],
+        use: ['style-loader', 'css-loader', 'sass-loader']
       }
-    ],
+    ]
   },
   devServer: {
-    contentBase: __dirname + '/public',
+    historyApiFallback: true,
   },
   plugins: [
-    new CleanWebpackPlugin(['css/main.css', 'js/bundle.js'], {
-      root: __dirname + '/public',
-      verbose: true,
-      dry: false, // true for simulation
-    }),
-  ],
+  new HtmlWebpackPlugin({
+    template: 'public/index.html'
+  })
+]
 };
